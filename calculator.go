8b7e3a6c13cdc40e001f5b59575bcade867903be
Rahul Kumar <rahul.kumar@nutanix.com>
2019-06-21 15:57:13 -0700

    calculator

---
+++ calculator.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(a int, b int)
+{
+	return a+b
+}
+
+func minus(a int, b int)
+{
+        return a+b
+}
+
+func mul(a int, b int)
+{
+        return a*b
+}
+
+func divide(a int, b int)
+{
+        return a/b
+}
+
+func main(){
+	a := 10
+	b := 5
+	
+	fmt.Println("a =10\t b=5")
+	fmt.Println("Add a+b :",a+b)
+	fmt.Println("Minus a-b :",a-b)
+	fmt.Println("Multiply a*b :",a*b)
+	fmt.Println("Divide a/b :",a/b
+
+	return)
+}
+
+
